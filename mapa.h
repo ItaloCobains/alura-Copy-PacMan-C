b48c1e8d7a5199a43697254f1e82d9ccb32cd590
Italo <italobrandao.contato@gmail.com>
2022-07-08 09:06:00 -0300

    create AI of Ghost

--- mapa.h
+++ mapa.h
@@ -30,3 +30,4 @@ int ehvalida(MAPA *m, int x, int y);
 int ehvazia(MAPA *m, int x, int y);
 void andanomapa(MAPA *m, int xorigem, int yorigem, int xdestino, int ydestino);
 void copiamapa(MAPA *destino, MAPA *origem);
+int praondeofantasmavai(int xatual, int yatual, int *xdestino, int *ydestino);
--- pacman.c
+++ pacman.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pacman.h"
 #include "mapa.h"
 
 MAPA m;
 POSICAO heroi;
+
+int praondeofantasmavai(int xatual, int yatual, int* xdestino, int* ydestino)
+{
+    int opcoes[4][2] = {
+        {xatual, yatual + 1},
+        {xatual + 1, yatual},
+        {xatual, yatual - 1},
+        {xatual - 1, yatual}};
+
+    srand(time(0));
+
+    for (int i = 0; i < 10; i++)
+    {
+        int posicao = rand() % 4;
+        if (ehvalida(&m, opcoes[posicao][0], opcoes[posicao][1]) && ehvazia(&m, opcoes[posicao][0], opcoes[posicao][1]))
+        {
+            *xdestino = opcoes[posicao][0];
+            *ydestino = opcoes[posicao][1];
+            return 1;
+        }
+    }
+    return 0;
+}
 
 void fantasmas()
 {
@@ -18,9 +42,14 @@ void fantasmas()
         {
             if (copia.matriz[i][j] == FANTASMA)
             {
-                if (ehvalida(&m, i, j + 1) && ehvazia(&m, i, j + 1))
+                int xdestino;
+                int ydestino;
+
+                int encontrou = praondeofantasmavai(i, j, &xdestino, &ydestino);
+
+                if (encontrou)
                 {
-                    andanomapa(&m, i, j, i, j + 1);
+                    andanomapa(&m, i, j, xdestino, ydestino);
                 }
             }
         }
